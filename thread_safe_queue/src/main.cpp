603125706ea62a3504a70b9095ec7b1fc63868aa
yurai007 <dawid_jurek@vp.pl>
2018-10-08 13:14:00 +0200

    thread_safe_queue: cleanup plus more queues for future use

--- thread_safe_queue/src/main.cpp
+++ thread_safe_queue/src/main.cpp
@@ -1,26 +1,20 @@
-﻿#include <iostream>
+﻿#include "thread_safe_queue.hpp"
+#include <iostream>
 #include <atomic>
 #include <cassert>
 #include <pthread.h>
-#include "thread_safe_queue.hpp"
 
-namespace thread_safe_queue_sct
-{
+constexpr auto max_consumers_number = 128u;
+constexpr auto iterations = 1000000;
+constexpr auto consumers_number = 4;
+using lli = long long int;
 
-#define MAX_CONSUMERS_SIZE 128
-const int iterations = 1000000;
-#define CONSUMERS_NUMBER 4
+static std::atomic<bool> finished {false};
+static pthread_mutex_t stdio_mutex = PTHREAD_MUTEX_INITIALIZER;
+static lli sums[max_consumers_number];
+static thread_safe_queue<int> queue;
 
-typedef long long int lli;
-
-std::atomic<bool> finished {false};
-
-pthread_mutex_t stdio_mutex = PTHREAD_MUTEX_INITIALIZER;
-lli sums[MAX_CONSUMERS_SIZE];
-
-framework::thread_safe_queue<int> queue;
-
-void check_errors(int result, const char *message)
+static void check_errors(int result, const char *message)
 {
     if (result < 0)
     {
@@ -29,7 +23,7 @@ void check_errors(int result, const char *message)
     }
 }
 
-void* producer(void *)
+static void* producer(void *)
 {
     lli all_pushed = iterations*(iterations-1L)/2L;
     printf("Sum of all pushed items = %lld\n", all_pushed);
@@ -38,15 +32,15 @@ void* producer(void *)
         queue.push(i);
     }
     finished = true;
-    pthread_exit(NULL);
+    pthread_exit(nullptr);
 }
 
-void* consumer(void *id)
+static void* consumer(void *id)
 {
+    auto thread_id = *static_cast<int*>(id);
     lli sum = 0;
-    int thread_id = *((int*)id);
 
-    while (finished == false || !queue.is_empty())
+    while (!finished || !queue.empty())
     {
         int value = 0;
         queue.try_pop(&value);
@@ -56,34 +50,45 @@ void* consumer(void *id)
     printf("Sum of popped items by consumer %d = %lld\n", thread_id, sum);
     pthread_mutex_unlock(&stdio_mutex);
     sums[thread_id] = sum;
-    pthread_exit(NULL);
+    pthread_exit(nullptr);
 }
 
-void test_case()
+static void test_memcheck()
 {
     std::cout << "Running sct_thread_safe_queue tests...\n";
     pthread_t producer_struct;
-    int producer_ids = CONSUMERS_NUMBER;
-    int rc = pthread_create(&producer_struct, NULL, producer, &producer_ids);
-    check_errors(rc, "Error creating the consumer thread..");
+    auto producer_ids = consumers_number;
+    auto rc = pthread_create(&producer_struct, nullptr, producer, &producer_ids);
+    check_errors(rc, "Error creating the producer thread..");
+    pthread_join(producer_struct, nullptr);
+    std::cout << "Verdict: OK\n";
+}
 
-    pthread_t consumers_struct[CONSUMERS_NUMBER];
-    int consumers_ids[CONSUMERS_NUMBER];
-    for (int i = 0; i < CONSUMERS_NUMBER; i++)
+static void test_case()
+{
+    std::cout << "Running sct_thread_safe_queue tests...\n";
+    pthread_t producer_struct;
+    auto producer_ids = consumers_number;
+    auto rc = pthread_create(&producer_struct, nullptr, producer, &producer_ids);
+    check_errors(rc, "Error creating the producer thread..");
+
+    pthread_t consumers_struct[consumers_number];
+    int consumers_ids[consumers_number];
+    for (int i = 0; i < consumers_number; i++)
     {
         consumers_ids[i] = i;
         sums[i] = 0;
-        rc = pthread_create(&consumers_struct[i], NULL, consumer, (void *)&consumers_ids[i]);
+        rc = pthread_create(&consumers_struct[i], nullptr, consumer, (void *)&consumers_ids[i]);
         check_errors(rc, "Error creating the consumer thread..");
     }
 
     lli sum = 0;
-    for (int i = 0; i < CONSUMERS_NUMBER; i++)
+    for (int i = 0; i < consumers_number; i++)
     {
-        pthread_join(consumers_struct[i], NULL);
+        pthread_join(consumers_struct[i], nullptr);
         sum += sums[i];
     }
-    pthread_join(producer_struct, NULL);
+    pthread_join(producer_struct, nullptr);
 
     printf("sums = %lld\n", sum);
     assert(sum == 499999500000);
@@ -91,11 +96,10 @@ void test_case()
     std::cout << "All sct_thread_safe_queue tests passed\n";
 }
 
-}
-
 int main()
 {
-    thread_safe_queue_sct::test_case();
+    test_case();
+    test_memcheck();
     return 0;
 }
 
--- thread_safe_queue/src/thread_safe_queue.hpp
+++ thread_safe_queue/src/thread_safe_queue.hpp
@@ -3,45 +3,54 @@
 #include <cstdio>
 #include <cstdlib>
 #include <pthread.h>
-#include <errno.h>
+#include <cerrno>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
 
-namespace framework
+template<class T>
+class std_thread_safe_queue
 {
+public:
+    void push(T new_value)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(std::move(new_value));
+        cv_.notify_one();
+    }
+
+    std::shared_ptr<T> front_and_pop()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        cv_.wait(lock, [this]{ return !queue_.empty(); });
+        auto result = std::make_shared<T>(std::move(queue_.front()));
+        queue_.pop();
+        return result;
+    }
+private:
+    mutable std::mutex mutex_;
+    std::queue<T> queue_;
+    std::condition_variable cv_;
+};
+
 
 template<class T>
 class thread_safe_queue
 {
 public:
-
-    thread_safe_queue()
-        : mutex(PTHREAD_MUTEX_INITIALIZER)
-    {
-        head = NULL;
-        tail = NULL;
-    }
-
-    ~thread_safe_queue()
-    {
-        // TO DO:
-    }
-
-    bool is_empty()
-    {
-        pthread_mutex_lock(&mutex);
-        bool result = head == NULL;
-        pthread_mutex_unlock(&mutex);
-        return result;
-    }
+    thread_safe_queue() = default;
+    thread_safe_queue(const thread_safe_queue&) = delete;
+    thread_safe_queue& operator=(const thread_safe_queue&) = delete;
 
     void push(T new_value)
     {
-        node *new_tail = new node;
+        auto new_tail = new node;
 
         pthread_mutex_lock(&mutex);
-        new_tail->next = NULL;
+        new_tail->next = nullptr;
         new_tail->value = new_value;
 
-        if (tail != NULL)
+        if (tail != nullptr)
             tail->next = new_tail;
         else
             head = new_tail;
@@ -52,35 +61,45 @@ public:
     bool try_pop(T *value)
     {
         pthread_mutex_lock(&mutex);
-        if (internal_is_empty())
+        if (internal_empty())
         {
             pthread_mutex_unlock(&mutex);
             return false;
         }
         *value = head->value;
-        node *old_head = head;
+        auto old_head = head;
         head = head->next;
         if (tail == old_head)
-            tail = NULL;
+            tail = nullptr;
 
         delete old_head;
         pthread_mutex_unlock(&mutex);
         return true;
     }
 
-private:
-    void check_errors(int result, const char *message)
+    bool empty() const
     {
-        if (result < 0)
+        pthread_mutex_lock(&mutex);
+        auto empty = head == nullptr;
+        pthread_mutex_unlock(&mutex);
+        return empty;
+    }
+
+    ~thread_safe_queue()
+    {
+        auto next = head;
+        while (next != nullptr)
         {
-            perror(message);
-            exit(errno);
+            auto nn = next->next;
+            delete next;
+            next = nn;
         }
     }
 
-    bool internal_is_empty()
+private:
+    bool internal_empty() const
     {
-        return head == NULL;
+        return head == nullptr;
     }
 
     struct node
@@ -89,8 +108,77 @@ private:
         T value;
     };
 
-    node *head, *tail;
-    pthread_mutex_t mutex;
+    node *head {nullptr}, *tail {nullptr};
+    mutable pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 };
 
-}
+template<class T>
+class fast_queue
+{
+    struct node
+    {
+        node *next;
+        T value;
+    };
+
+public:
+    fast_queue() :
+        head(new node),
+        tail(head)
+    {
+        tail->next = nullptr;
+    }
+
+    node *get_tail()
+    {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    // now we store new_value to current tail and create new empty dummy tail.
+    // Notice we don't touch head but we may touch value pointing by head (if head == tail)
+    void push(T new_value)
+    {
+        node *new_dummy_tail = new node;
+        new_dummy_tail->next = nullptr;
+
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        tail->value = new_value;
+        tail->next = new_dummy_tail;
+        tail = new_dummy_tail;
+    }
+
+    node *pop_head()
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if (head == get_tail())
+            return nullptr;
+        node *old_head = head;
+        head = head->next;
+        return old_head;
+    }
+
+    bool try_pop(T &value)
+    {
+        node *old_head = pop_head();
+        if (old_head != nullptr)
+        {
+            value = old_head->value;
+            delete old_head;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    bool empty()
+    {
+        return head == tail;
+    }
+
+private:
+    node *head, *tail;
+    std::mutex head_mutex;
+    std::mutex tail_mutex;
+};
+
