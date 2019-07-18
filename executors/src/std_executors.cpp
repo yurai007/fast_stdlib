5ea3335c62edea24dbfc64dd677dba5de6b2df7d
yurai007 <dawid_jurek@vp.pl>
2019-07-18 11:18:37 +0200

    executors tests from consensus repo

--- executors/src/std_executors.cpp
+++ executors/src/std_executors.cpp
@@ -257,6 +257,160 @@ static auto Ubsan_minimal_reproducer1() {
 }
 }
 
+#define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
+
+template <class Executor, class Function>
+#ifndef __clang__
+requires requires(Executor e, Function f) {
+   // CopyConstructible<Executor>;
+    e.execute(f);
+}
+#endif
+inline void async1(Executor &ex, Function &&f) {
+    execution::require(ex, execution::single, execution::oneway).execute(FWD(f));
+}
+
+template <class Executor, class Function>
+#ifndef __clang__
+#endif
+inline auto async(Executor ex, Function &&f) {
+    return execution::require(ex, execution::twoway).twoway_execute(FWD(f));
+}
+
+namespace test_diagnostics_improvements_with_concepts {
+
+class inline_executor {
+public:
+#if 0
+    inline_executor() = default;
+    inline_executor(const inline_executor&) = delete;
+    inline_executor &operator=(inline_executor&) = delete;
+#endif
+    friend bool operator==(const inline_executor&, const inline_executor&) noexcept {
+        return true;
+    }
+
+    friend bool operator!=(const inline_executor&, const inline_executor&) noexcept {
+        return false;
+    }
+/* Here with 'if 0 execute' concept in async1 helps a lot comparing to async:
+
+   ../../src/std_executors.cpp:269:13: note:   constraints not satisfied
+    inline void async1(Executor &ex, Function &&f) {
+
+    VS
+    one screen from executor internals
+*/
+
+#if 0
+#endif
+    template <class Function>
+    inline void execute(Function &&f) const noexcept {
+        f();
+    }
+};
+
+static auto test() {
+    static_assert(execution::is_oneway_executor_v<inline_executor>);
+    inline_executor executor;
+    auto done = false;
+    async1(executor, [&done]{
+        done = true;
+    });
+    assert(done);
+}
+
+}
+
+namespace more_tests {
+
+#if 0
+/*
+ * First one is broken - from some reason setting before getting cause Asan complains
+ * Second one is not correct - returned future is not ready
+*/
+template<class T>
+[[nodiscard]]
+inline auto make_ready_future__broken(T &&value) {
+    promise<T> p;
+    p.set_value(value);
+    return p.get_future();
+}
+
+template<class T>
+[[nodiscard]]
+inline auto make_ready_future__invalid(T &&value) {
+    return execution::require(pool.executor(), execution::never_blocking).twoway_execute([value]{
+        return value;
+    });
+}
+#endif
+
+static auto test() {
+    static_thread_pool pool(1);
+    // graceful shutdown (joining in wait())
+    {
+        static_thread_pool _pool(1);
+        auto agent = execution::require(_pool.executor(), execution::single, execution::oneway);
+        auto done = false;
+        agent.execute([&done]{
+            done = true;
+        });
+        _pool.wait();
+        assert(done);
+    }
+    // force shutdown (no joins)
+    {
+        static_thread_pool _pool(1);
+        auto agent = execution::require(_pool.executor(), execution::single, execution::oneway);
+        auto done = false;
+        agent.execute([&done]{
+            done = true;
+        });
+        _pool.stop();
+        assert(!done);
+    }
+    {
+        auto agent = execution::require(pool.executor(), execution::twoway);
+        auto f = agent.twoway_execute([]{});
+        assert(f.valid());
+        f.get();
+        assert(!f.valid());
+    }
+    {
+        auto f = execution::require(pool.executor(), execution::oneway).twoway_execute([](){
+            return 42;
+        });
+        assert(f.get() == 42);
+    }
+#if 0
+    {
+        static_assert(execution::is_oneway_executor_v<inline_executor>);
+        inline_executor executor;
+        auto agent = execution::require(executor, execution::oneway, execution::single);
+        auto done = false;
+        agent.execute([&done]{
+            done = true;
+        });
+        assert(done);
+    }
+#endif
+    {
+        auto f = execution::require(pool.executor(), execution::twoway).twoway_execute([](){
+            return 42;
+        }).then([](auto maybe_value){
+            auto value = maybe_value.get();
+            return ++value;
+        }).then([](auto maybe_value){
+            auto value = maybe_value.get();
+            return ++value;
+        });
+        assert(f.get() == 44);
+    }
+    pool.wait();
+}
+}
+
 int main() {
     properties::preliminaries_without_concepts();
 #ifndef __clang__
@@ -264,6 +418,8 @@ int main() {
 #endif
     polymorphic_executors::test();
     properties::test_continuation_property();
+    test_diagnostics_improvements_with_concepts::test();
+    more_tests::test();
     experimental_bugs::Ubsan_minimal_reproducer1();
     properties::test_outstanding_work();
     return 0;
